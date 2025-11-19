0f39528d3bb1cd8d6096ff9723c778719d47ed0c
Monalize M <150396638+mlize08@users.noreply.github.com>
2025-11-18 21:08:54 -0300

    condicional

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let texto = document.querySelector("#texto");
 
-
 let nome = prompt("Qual o seu nome?");
-texto.textContent = nome;
+if(nome == null){
+    texto.textContent = "Seja bem-vindo(a)!";
+}
+else{
+    texto.textContent = nome;
+}
+
